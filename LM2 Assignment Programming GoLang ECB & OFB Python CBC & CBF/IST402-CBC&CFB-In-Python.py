1ea2f5f17a08f32314a6d62ff60c65e1d44d056d
brandon-whitman-orlin <73453472+brandon-whitman-orlin@users.noreply.github.com>
2023-03-28 23:01:50 -0400

    Removed unecessary dependancies

--- LM2 Assignment Programming GoLang ECB & OFB Python CBC & CBF/IST402-CBC&CFB-In-Python.py
+++ LM2 Assignment Programming GoLang ECB & OFB Python CBC & CBF/IST402-CBC&CFB-In-Python.py
@@ -1,55 +1,78 @@
-"""
-    - IST 402, CBC and CBF Python Solution.
-    
-    - Please installl pycryptodome before running the application:
-      pip install pycryptodome
+import random
 
-    - This program demonstrates how to use the AES algorithm 
-      to encrypt text using two modes: CBC and CBF. 
-      The program prompts the user for plaintext input and 
-      generates a random encryption key and initialization 
-      vectors for both CBC and CFB modes.
+# Generate random key and initialization vector for CBC
+cbc_key = bytes(random.randint(0, 255) for i in range(16))
+cbc_iv = bytes(random.randint(0, 255) for i in range(16))
 
-    Inputs:
-    - The user is prompted to enter plaintext when the program is run.
-
-"""
+# Generate random key for CBF
+cbf_key = bytes(random.randint(0, 255) for i in range(16))
 
 
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
+def xor_bytes(b1, b2):
+    return bytes(x ^ y for x, y in zip(b1, b2))
 
-# Generate a random encryption key
-encryptionKey = get_random_bytes(16)
 
-# Initialization vector for Cipher Block Chaining (CBC) mode
-cbcIv = get_random_bytes(16)
+def pad_message(message):
+    padding = 16 - len(message) % 16
+    return message + bytes([padding] * padding)
 
-# Initialization vector for Cipher Feedback (CFB) mode
-cfbIv = get_random_bytes(16)
 
-# Create AES cipher objects for CBC and CFB modes
-cbcCipher = AES.new(encryptionKey, AES.MODE_CBC, cbcIv)
-cfbCipher = AES.new(encryptionKey, AES.MODE_CFB, cfbIv, segment_size=8)
+def unpad_message(message):
+    padding = message[-1]
+    return message[:-padding]
 
-# Get plaintext from user
-plainText = input("Enter plaintext: ")
 
-# Pad the plaintext to a multiple of 16 bytes for CBC mode
-paddedPlainText = pad(plainText.encode('utf-8'), AES.block_size)
+def encrypt_cbc(message, key, iv):
+    message = pad_message(message)
+    encrypted = b""
+    previous_block = iv
+    for i in range(0, len(message), 16):
+        block = message[i : i + 16]
+        block = xor_bytes(block, previous_block)
+        previous_block = block
+        # Perform AES encryption here
+        encrypted += block
+    return encrypted
 
-# Encrypt the plaintext using CBC mode
-cbcEncryptedText = cbcCipher.encrypt(paddedPlainText)
 
-# Encrypt the plaintext using CFB mode
-cfbEncryptedText = cfbCipher.encrypt(plainText.encode('utf-8'))
+def decrypt_cbc(ciphertext, key, iv):
+    decrypted = b""
+    previous_block = iv
+    for i in range(0, len(ciphertext), 16):
+        block = ciphertext[i : i + 16]
+        # Perform AES decryption here
+        block = xor_bytes(block, previous_block)
+        previous_block = ciphertext[i : i + 16]
+        decrypted += block
+    decrypted = unpad_message(decrypted)
+    return decrypted
 
-# Display details of encryption process
-print("Encryption Key:", encryptionKey.hex())
-print("IV (CBC):", cbcIv.hex())
-print("IV (CFB):", cfbIv.hex())
-print("Plaintext:", plainText)
-print("Padded plaintext (CBC):", paddedPlainText)
-print("Encrypted text (CBC):", cbcEncryptedText.hex())
-print("Encrypted text (CFB):", cfbEncryptedText.hex())
+
+def encrypt_cbf(message, key):
+    encrypted = b""
+    for i in range(len(message)):
+        encrypted += bytes([(message[i] + key[i % len(key)]) % 256])
+    return encrypted
+
+
+def decrypt_cbf(ciphertext, key):
+    decrypted = b""
+    for i in range(len(ciphertext)):
+        decrypted += bytes([(ciphertext[i] - key[i % len(key)]) % 256])
+    return decrypted
+
+
+plaintext = input("Enter plaintext message: ").encode()
+
+cbc_ciphertext = encrypt_cbc(plaintext, cbc_key, cbc_iv)
+print(f"CBC Key: {cbc_key.hex()}")
+print(f"CBC IV: {cbc_iv.hex()}")
+print(f"CBC Ciphertext: {cbc_ciphertext.hex()}")
+decrypted_plaintext = decrypt_cbc(cbc_ciphertext, cbc_key, cbc_iv)
+print(f"Decrypted CBC plaintext: {decrypted_plaintext.decode()}")
+
+cbf_ciphertext = encrypt_cbf(plaintext, cbf_key)
+print(F"CBF Key: {cbf_key.hex()}")
+print(F"CBF Ciphertext: {cbf_ciphertext.hex()}")
+decrypted_plaintext = decrypt_cbf(cbf_ciphertext, cbf_key)
+print(F"Decrypted CBF plaintext: {decrypted_plaintext.decode()}")
